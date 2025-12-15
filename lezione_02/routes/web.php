e880316073b55084aff7ce16eef70454d325b596
spadafinasara <spadafina.sara@gmail.com>
2025-12-16 00:28:33 +0100

    inizio esercitazione 15-12

--- lezione_02/routes/web.php
+++ lezione_02/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chi siamo', function () {
+    $nomi = ["Matteo", "Maria", "Maryna", "Elisabetta", "Simone"];
+    return view('chiSiamo', ["names" => $nomi]);
+
+});
